d3099093ad23ad565236fc5012b549a44efce67c
TheSabiin <36886400+TheSabiin@users.noreply.github.com>
2018-05-06 23:45:23 -0300

    Add files via upload

---
+++ MostraNome/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MostraNome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um nome: ");
+            String nome = Console.ReadLine();
+            Console.WriteLine(nome + " Brilha muito no Corinthians");
+        }
+    }
+}
